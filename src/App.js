1f8539e7ad0710925e1c9eb5d1862ec92027349b
nikunjjasani1 <nikunjjasnai1@gmail.com>
2023-07-11 01:21:28 +0530

    devlopment

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+   <h1>hello world</h1>
+   <h1>hello world</h1>
+   <h1>hello world</h1>
+   <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
